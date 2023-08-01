59d08eeac15cb51c1f1a9b7d1982f59e30b97607
Semhal22 <semhalgidena@gmail.com>
2023-08-01 23:13:34 +0000

    Delete an entire binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -26,5 +26,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 int binary_tree_is_leaf(const binary_tree_t *node);
+void binary_tree_delete(binary_tree_t *tree);
 
 #endif /*BINARY_TREES_H*/

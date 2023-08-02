3a8ad1115d32792782a6c3229ccb775cf0768b12
uelkariuki <cratzuel@gmail.com>
2023-08-02 00:40:51 +0000

    Task 6. Pre-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -15,7 +15,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (func == NULL)
 		return;
 
-	printf("%d \n", tree->n);
+	printf("%d\n", tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
 

7701c95c23881dfdb993f395f8b33e78b4f06a25
uelkariuki <cratzuel@gmail.com>
2023-08-03 23:44:32 +0000

    Task 28.1  BST - Remove

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -4,7 +4,8 @@
  * bst_remove- a function that removes a node from a Binary Search Tree
  * @root: a pointer to the root node of the tree where you will remove a node
  * @value: the value to remove in the tree
- * Return: a pointer to the new root node of the tree after removing the desired value
+ * Return: a pointer to the new root node of the tree after removing the
+ * desired value
  *
 */
 bst_t *bst_remove(bst_t *root, int value)
@@ -13,12 +14,10 @@ bst_t *bst_remove(bst_t *root, int value)
 
 	if (root == NULL)
 		return (NULL);
-
 	if (value < root->n) /*if value less delete from the left*/
 		root->left = bst_remove(root->left, value);
 	else if (value > root->n)
 		root->right = bst_remove(root->right, value);
-
 	else /*when the node to be deleted is found*/
 	{
 		if (root->left == NULL) /*if node to be deleted has only one child*/
@@ -26,7 +25,6 @@ bst_t *bst_remove(bst_t *root, int value)
 			temp = root->right;
 			free(root);
 			return (temp);
-
 		}
 		else if (root->right == NULL)
 		{
@@ -37,15 +35,11 @@ bst_t *bst_remove(bst_t *root, int value)
 		/*if the node to be removed has two children*/
 		temp = root->right;
 		while (temp && temp->right != NULL)
-		{
 			temp = temp->left;
-		}
 		/*found the in-order successor, replace value of to be removed node*/
 		/*with the value in the in-order successor*/
 		root->n = temp->n;
 		root->right = bst_remove(root->right, temp->n); /*remove the node*/
-
-
 	}
 	return (root);
 

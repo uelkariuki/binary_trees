c229482d926aef350775fd61ae7c30225fd6f70d
Semhal22 <semhalgidena@gmail.com>
2023-08-03 21:12:36 +0000

    Check if a binary tree is complete

--- binary_trees.h
+++ binary_trees.h
@@ -46,6 +46,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 const binary_tree_t *second);
+int binary_tree_is_complete(const binary_tree_t *tree);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);

157f6218445400af189c8e914969bc40c21a1042
uelkariuki <cratzuel@gmail.com>
2023-08-04 02:44:54 +0000

    Task 36. Is Binary heap

---
+++ 130-binary_tree_is_heap.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+bool is_heap(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (true);
+
+	if (tree->left && tree->left->n >= tree->n)
+		return (false);
+
+	if (tree->right && tree->right->n <= tree->n)
+		return (false);
+
+
+	return (is_heap(tree->left) && is_heap(tree->right));
+}
+/**
+ * binary_tree_is_heap- function that checks if a binary tree is a valid
+ * Max Binary Heap
+ * @tree: a pointer to the root node of the tree to check
+ * Return: 1 if tree is a valid Max Binary Heap, and 0 otherwise
+ *
+*/
+int binary_tree_is_heap(const binary_tree_t *tree)
+{
+
+	if (tree == NULL)
+		return (0);
+
+	return (is_heap(tree));
+
+}
--- binary_trees.h
+++ binary_trees.h
@@ -59,6 +59,8 @@ int binary_tree_is_avl(const binary_tree_t *tree);
 bst_t *array_to_bst(int *array, size_t size);
 avl_t *array_to_avl(int *array, size_t size);
 bst_t *bst_remove(bst_t *root, int value);
+int binary_tree_is_heap(const binary_tree_t *tree);
+bool is_heap(const binary_tree_t *tree);
 
 
 #endif /*BINARY_TREES_H*/

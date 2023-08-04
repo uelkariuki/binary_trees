ebf1b20b59429944092440f54280fbcc3a263cef
uelkariuki <cratzuel@gmail.com>
2023-08-04 22:16:39 +0000

    update Task 28

---
+++ 121-avl_insert.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * avl_insert- function that inserts a value in an AVL Tree
+ * @tree: a double pointer to the root node of the AVL tree for inserting
+ * the value
+ * @value: the value to store in the node to be inserted
+ * Return: a pointer to the created node, or NULL on failure
+ *
+*/
+
+avl_t *avl_insert(avl_t **tree, int value)
+{
+	(void) tree;
+	(void) value;
+
+	return (tree);
+
+}
--- binary_trees.h
+++ binary_trees.h
@@ -64,5 +64,6 @@ int binary_tree_is_heap(const binary_tree_t *tree);
 bool is_heap(const binary_tree_t *tree);
 heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
+avl_t *avl_insert(avl_t **tree, int value);
 
 #endif /*BINARY_TREES_H*/
